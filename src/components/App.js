8852f0c682367d0891a45fc45947de0b2a3b89ed
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-07-10 05:13:13 +0530

    adding propTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,85 @@
-import React  from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import {fetchPosts} from '../actions/posts'
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
 
 class App extends React.Component {
-  //dispatch an action
   componentDidMount() {
     this.props.dispatch(fetchPosts());
   }
-  
+
   render() {
-    console.log("PROPS ",this.props);
+    const { posts } = this.props;
     return (
       <div>
-        App
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    posts:state.posts,
+    posts: state.posts,
   };
 }
+//property of an App
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
 
-
-
-export default connect(mapStateToProps)(App)
-
+export default connect(mapStateToProps)(App);
