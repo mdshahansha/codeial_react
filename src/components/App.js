995a340e13879faf933ce614740c68e2cf942d1d
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-07-09 17:00:09 +0530

    fetching posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
-function App(){
-  return <div className="App">App Done</div>
+import React  from 'react'
+import { connect } from 'react-redux'
+
+import {fetchPosts} from '../actions/posts'
+
+class App extends React.Component {
+  //dispatch an action
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+  
+  render() {
+    console.log("PROPS ",this.props);
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
 }
- 
-export default App;
+
+function mapStateToProps(state){
+  return {
+    posts:state.posts,
+  };
+}
+
+
+
+export default connect(mapStateToProps)(App)
+
