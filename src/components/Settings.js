ed593adca78cc308c6ec5e1af9218f55c59f5df9
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-07-13 15:35:33 +0530

    adding settings UI

---
+++ src/components/Settings.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      password: "",
+      confirmPassword: "",
+      editMode: false,
+    };
+  }
+  render() {
+    const { user } = this.props.auth;
+    return (
+      <div className="settings">
+        <div className="img-container">
+          <img
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-dp"
+            id="user-dp"
+          />
+        </div>
+        <div className="field">
+          <div className="field-label">Email</div>
+          <div className="field-value">{user.email}</div>
+        </div>
+
+        <div className="field">
+          <div className="field-label">Name</div>
+          {editMode ? (
+            <input
+              type="text"
+              onChange={() => this.handleChange()}
+              value={this.state.name}
+            />
+          ) : (
+            <div className="field-value">{user.email}</div>
+          )}
+        </div>
+        {editMode && (
+          <div className="field">
+            <div className="field-label">New password</div>
+
+            <input
+              type="password"
+              onChange={() => this.handleChange()}
+              value={this.state.password}
+            />
+          </div>
+        )}
+
+        {editMode && (
+          <div className="field">
+            <div className="field-label">Confirm password</div>
+
+            <input
+              type="password"
+              onChange={() => this.handleChange()}
+              value={this.state.confirmPassword}
+            />
+          </div>
+        )}
+
+            <div className="btn-grp">
+                {editMode ? (<button className="button save-btn">Save</button>
+     ):(
+        <button className="button save-btn">Edit profile</button>   
+     )}
+
+        {editMode && <div className="go-back">
+            Go Back
+        </div> }
+
+            </div>
+
+      </div>
+    );
+  }
+}
+
+export default Settings;
